955f84328013d4c0f361776a0e165da37dd58040
Viktor Kovacs <crusader8@gmail.com>
2017-07-05 20:19:45 +0200

    make examples work

--- src/main/java/hu/crs/App.java
+++ src/main/java/hu/crs/App.java
@@ -8,9 +8,19 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
         Gson gson = new Gson();
-        Map<String, String> map = new HashMap<>();
-        map.put("a", "1");
-        System.out.println("Ok: " + gson.toJson(map));
-        System.out.println("Not Ok: " + gson.toJson(new HashMap<String, String>(){{put("b", "2");}}));
+        Map<String, String> okMap = new HashMap<>();
+        //1
+        okMap.put("a", "1");
+        System.out.println("Ok: " + gson.toJson(okMap));
+
+        //2
+        Map<String, String> notOkMap = new HashMap<String, String>() {{
+            put("b", "2");
+        }};
+        System.out.println("Not Ok: " + gson.toJson(notOkMap));
+
+        //3
+        MapChildrenClass mapChildrenClass = new MapChildrenClass();
+        System.out.println("Map children class: " + gson.toJson(mapChildrenClass));
     }
 }
---
+++ src/main/java/hu/crs/MapChildrenClass.java
@@ -0,0 +1,9 @@
+package hu.crs;
+
+import java.util.HashMap;
+
+public class MapChildrenClass extends HashMap<String, String> {
+    {
+        put("C", "3");
+    }
+}
